747d3f8708bd56f7a5f0e488539567cfc7482d74
Julian <julian.sobott@gmail.com>
2019-10-19 11:59:18 +0200

    Add Constructor to Ship

--- src/core/Ship.java
+++ src/core/Ship.java
@@ -5,8 +5,19 @@ public class Ship extends PlaygroundElement{
     private int lives;
     private int id;
 
+    private static int nextID = 0;
+
     public enum LifeStatus {
         SUNKEN, ALIVE
+    }
+
+    Ship(int lives){
+        this.lives = lives;
+        this.id = this.generateID();
+    }
+
+    private int generateID() {
+        return Ship.nextID++;
     }
 
     LifeStatus getStatus() {
@@ -20,5 +31,9 @@ public class Ship extends PlaygroundElement{
     public void gotHit() {
         super.gotHit();
         this.lives--;
+    }
+
+    public int getId() {
+        return id;
     }
 }
