13767596d4509b5c4b2d9ce46bb8a8360295ec65
Julian <julian.sobott@gmail.com>
2019-10-19 12:05:03 +0200

    Implement class PLaceShipResult

--- src/core/communication_data/PlaceShipResult.java
+++ src/core/communication_data/PlaceShipResult.java
@@ -1,4 +1,20 @@
 package core.communication_data;
 
-public interface PlaceShipResult {
+class PlaceShipResult {
+
+    private final boolean successfullyPlaced;
+    private final ShipPosition position;
+
+    public PlaceShipResult(boolean successfullyPlaced, ShipPosition position) {
+        this.successfullyPlaced = successfullyPlaced;
+        this.position = position;
+    }
+
+    public boolean isSuccessfullyPlaced() {
+        return successfullyPlaced;
+    }
+
+    public ShipPosition getPosition() {
+        return position;
+    }
 }
