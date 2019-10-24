68d34b45d2208fa5f4781cd3d50b90a8edb63d48
lukas <lukasmendel14043@gmail.com>
2019-10-24 21:57:50 +0200

    add new gui logic

--- src/gui/newGame/ControllerGameType.java
+++ src/gui/newGame/ControllerGameType.java
@@ -3,14 +3,29 @@ package gui.newGame;
 import gui.ControllerMainMenu;
 import gui.WindowChange.SceneLoader;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ControllerGameType {
+
+public class ControllerGameType implements Initializable {
 
     @FXML
     public Button BackToMenu;
+
+    @FXML
+    public RadioButton radioButtonKI;
+
+    @FXML
+    public RadioButton radioButtonNetzwerk;
+
+    @FXML
+    public  RadioButton radioButtonLocal;
 
 
 
@@ -24,7 +39,17 @@ public class ControllerGameType {
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        ToggleGroup toggleGroupGameType = new ToggleGroup();
+        radioButtonKI.setToggleGroup(toggleGroupGameType);
+        radioButtonNetzwerk.setToggleGroup(toggleGroupGameType);
+        radioButtonLocal.setToggleGroup(toggleGroupGameType);
+
+
+
+    }
 }
 
 
