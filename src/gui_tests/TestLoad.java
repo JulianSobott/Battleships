df4dba5f7a759a04d47577dee5c54fdbd5df85ee
Julian Sobott <julian.sobott@gmail.com>
2020-01-04 14:40:40 +0100

    [feature GUI] Add TestLoad class

---
+++ src/gui_tests/TestLoad.java
@@ -0,0 +1,31 @@
+package gui_tests;
+
+import core.communication_data.LoadGameResult;
+import core.serialization.GameData;
+import core.serialization.GameSerialization;
+import core.utils.logging.LoggerGUI;
+import gui.PlayGame.ControllerPlayGame;
+import gui.WindowChange.SceneLoader;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class TestLoad extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        int gameID = 1;
+        LoadGameResult res = GameSerialization.loadGame(gameID);
+        if (res.getStatus() == LoadGameResult.LoadStatus.SUCCESS) {
+            GameData gameData = res.getGameData();
+            ControllerPlayGame controller = ControllerPlayGame.fromLoad(gameData);
+            SceneLoader sceneLoader = new SceneLoader(null, "../../gui/PlayGame/PlayGame.fxml", controller);
+            sceneLoader.loadSceneInNewWindow("Test Ship Placement");
+        } else {
+            LoggerGUI.error("Load failed: res=" + res);
+        }
+    }
+}
