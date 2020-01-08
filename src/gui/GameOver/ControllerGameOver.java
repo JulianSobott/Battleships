a68732d49cf6383e8a29f3492b127294a75427b2
lukas <lukasmendel14043@gmail.com>
2020-01-08 22:17:35 +0100

    update GameOver Gui

--- src/gui/GameOver/ControllerGameOver.java
+++ src/gui/GameOver/ControllerGameOver.java
@@ -1,6 +1,8 @@
 package gui.GameOver;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -8,8 +10,20 @@ import java.util.ResourceBundle;
 
 
 public class ControllerGameOver implements Initializable {
+
+    @FXML
+    private Label labelGameStatus;
+
+
+    public ControllerGameOver(Boolean HumanWinner){
+
+
+    }
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
 
     }
 }
