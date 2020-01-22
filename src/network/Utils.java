7fb6924837639419110ed9964ab0380d93693e9c
JulianSobott <julian.sobott@gmail.com>
2020-01-22 15:51:30 +0100

    [feature network] Alternative solution fot GetIpAddress

--- src/network/Utils.java
+++ src/network/Utils.java
@@ -6,21 +6,19 @@ public class Utils {
 
     public static String getIpAddress() {
         String ip;
-        try {
-            ip = Inet4Address.getLocalHost().getHostAddress();
-        } catch (UnknownHostException e) {
+        try(final DatagramSocket socket = new DatagramSocket()){
+            socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+            ip = socket.getLocalAddress().getHostAddress();
+        } catch (UnknownHostException | SocketException e) {
             e.printStackTrace();
             return null;
         }
-        if (ip.equals("127.0.0.1")) {
-            try(final DatagramSocket socket = new DatagramSocket()){
-                socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
-                ip = socket.getLocalAddress().getHostAddress();
-            } catch (UnknownHostException | SocketException e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
         return ip;
+//        try {
+//            ip = Inet4Address.getLocalHost().getHostAddress();
+//        } catch (UnknownHostException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
     }
 }
